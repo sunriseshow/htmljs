33f9c25513c2906518d67ea1391d43a106c08081
sunxinyu <sunxinyu@souche.com>
2016-02-23 10:24:28 +0800

    timelines

--- models/timelines.js
+++ models/timelines.js
@@ -6,13 +6,30 @@
       autoIncrement: true,
       primaryKey: true
     },
-    who_id: "int",
-    who_headpic: "varchar(255)",
-    who_nick: "varchar(100)",
+    user_id: "int",
+    user_headpic: "varchar(255)",
+    user_nick: "varchar(100)",
+
+    target_title:"varchar(255)",
     target_url: "varchar(255)",
-    target_name: "varchar(100)",
-    action: "varchar(30)",
-    desc: "text"
+    target_desc: "varchar(100)",
+    target_pic:"varchar(255)",
+    target_id:"int", //目标id，文章或者评论的id
+    action: "varchar(30)", // 1分享了 2评论了 3发表了
+    desc: "text",
+
+    comment_count:{
+      type:"int",
+      defaultValue:0
+    },
+    zan_count:{
+      type:"int",
+      defaultValue:0
+    },
+    is_publish:{
+      type:"tinyint",
+      defaultValue:0
+    }
   };
 
 }).call(this);
